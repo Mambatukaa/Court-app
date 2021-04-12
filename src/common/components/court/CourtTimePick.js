702b606026f1d20f24ece671da28cfe80f3d85bc
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-04-12 22:24:07 +0800

    time pick update

--- src/common/components/court/CourtTimePick.js
+++ src/common/components/court/CourtTimePick.js
@@ -1,45 +1,128 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Button } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView
+} from 'react-native';
+
+const data = [
+  {
+    name: 'Monday',
+    list: [
+      {
+        time: '10:00',
+        leftPeople: 10
+      },
+      {
+        time: '12:00',
+        leftPeople: 10
+      },
+      {
+        time: '14:00',
+        leftPeople: 10
+      },
+      {
+        time: '16:00',
+        leftPeople: 10
+      },
+      {
+        time: '17:00',
+        leftPeople: 10
+      },
+      {
+        time: '17:00',
+        leftPeople: 10
+      },
+      {
+        time: '17:00',
+        leftPeople: 10
+      }
+    ]
+  },
+  {
+    name: 'Tuesday',
+    list: [
+      {
+        time: '10:00',
+        leftPeople: 10
+      },
+      {
+        time: '12:00',
+        leftPeople: 10
+      },
+      {
+        time: '14:00',
+        leftPeople: 10
+      },
+      {
+        time: '18:00',
+        leftPeople: 10
+      },
+      {
+        time: '20:00',
+        leftPeople: 10
+      },
+      {
+        time: '17:00',
+        leftPeople: 10
+      },
+      {
+        time: '17:00',
+        leftPeople: 10
+      }
+    ]
+  }
+];
 
 const CourtTimePick = props => {
+  const renderRowItem = (item, index) => {
+    return (
+      <View style={styles.block} key={index.toString()}>
+        <Text style={styles.time}>{item.time}</Text>
+        <Text style={styles.people}>{item.leftPeople}</Text>
+      </View>
+    );
+  };
+  const renderRow = (data, index) => {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.date}>{'Mon\n12'}</Text>
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+          key={index.toString()}
+        >
+          {data.list.map((e, i) => renderRowItem(e, i))}
+        </ScrollView>
+      </View>
+    );
+  };
+
   return (
     // props.time, props.active, props.booked, props.huniiToo
     <View>
-      <Text style={styles.weekChange}>March 25- April 01</Text>
-      <Text style={styles.date}>Thu 25</Text>
-    <ScrollView horizontal={true}>
-      <View style={styles.block}>
-        <Text style={styles.time}>13:00</Text>
-        <Text style={styles.people}>8</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Button title='prev' />
+        <Text>April 4-10</Text>
+        <Button title='next' />
       </View>
-      <View style={styles.block}>
-        <Text style={styles.time}>13:00</Text>
-        <Text style={styles.people}>8</Text>
-      </View>
-      <View style={styles.block}>
-        <Text style={styles.time}>13:00</Text>
-        <Text style={styles.people}>8</Text>
-      </View>
-      <View style={styles.block}>
-        <Text style={styles.time}>13:00</Text>
-        <Text style={styles.people}>8</Text>
-      </View>
-      <View style={styles.block}>
-        <Text style={styles.time}>13:00</Text>
-        <Text style={styles.people}>8</Text>
-      </View>
-      <View style={styles.block}>
-        <Text style={styles.time}>13:00</Text>
-        <Text style={styles.people}>8</Text>
-      </View>
-    </ScrollView>
+      <ScrollView>{data.map((e, i) => renderRow(e, i))}</ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   weekChange: {
-    borderWidth:1,
+    borderWidth: 1,
     borderRadius: 5,
     marginTop: 5,
     width: 300,
@@ -51,10 +134,10 @@ const styles = StyleSheet.create({
     overflow: 'hidden'
   },
   date: {
-  fontSize: 20,
-  fontWeight: 'bold',
-  marginTop: 10,
-  margin: 5
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+    margin: 5
   },
   block: {
     borderWidth: 1,
@@ -63,7 +146,6 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     width: 60,
     backgroundColor: 'white'
-
   },
   time: {
     textAlign: 'center',
