97400a69aaa3508274fd699dbebfe3d0895885c6
Bat-Amgalan Nasan-Ochir <sbatuka9@gmail.com>
2021-04-16 14:18:32 +0800

    update auth screens

--- src/screens/auth/SignIn.js
+++ src/screens/auth/SignIn.js
@@ -1,14 +1,97 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  TextInput
+} from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import GradientBtn from '../../common/components/GradientBtn';
+import { AuthContext } from '../../common/utils/AuthContext';
 
 const SignIn = props => {
+  const navigation = useNavigation();
+  const { signIn } = React.useContext(AuthContext);
+
   return (
-    <View>
-      <Text>Sign in screen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View />
+        <View style={{ justifyContent: 'center' }}>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 20,
+              textAlign: 'center',
+              paddingBottom: 20
+            }}
+          >
+            Нэвтрэх
+          </Text>
+
+          <TextInput
+            placeholder='Email'
+            keyboardType='email-address'
+            style={[styles.input, styles.email]}
+          />
+          <TextInput
+            placeholder='Password'
+            secureTextEntry={true}
+            style={[styles.input, styles.password]}
+          />
+          <Text style={{ marginTop: 15 }}>
+            <Text style={[styles.text]}>{'Хэрэв та бүртгүүлээгүй бол '}</Text>
+            <Text
+              onPress={() => {
+                navigation.navigate('SignUp');
+              }}
+              style={[{ color: '#B43CF3' }, styles.text]}
+            >
+              {'энд '}
+            </Text>
+            <Text style={[styles.text]}>{'дарна уу.'}</Text>
+          </Text>
+        </View>
+        <GradientBtn
+          linearGradientStyle={{ width: 160, borderRadius: 11 }}
+          text='Нэвтрэх'
+          onPress={() => {
+            signIn();
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  input: {
+    height: 50,
+    width: 250,
+    padding: 10,
+    borderWidth: 1.5
+  },
+
+  email: {
+    borderRadius: 10,
+    marginBottom: 15
+  },
+  password: {
+    borderRadius: 10
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 13
+  }
+});
 
 export default SignIn;
--- src/screens/auth/SignUp.js
+++ src/screens/auth/SignUp.js
@@ -1,14 +1,132 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 
+import Editbox from '../../common/components/Editbox';
+import LayoutView from '../../common/components/LayoutView';
+
+import { Ionicons } from '@expo/vector-icons';
+
+import { colors } from '../../common/styles';
+import { GradientBtn } from '../../common/components';
+
 const SignUp = props => {
+  const renderTop = () => {
+    return <View style={{ height: 50 }}></View>;
+  };
+  const renderBottom = () => {
+    return (
+      <View style={styles.container}>
+        <View
+          style={{
+            flexWrap: 'wrap',
+            marginTop: -80,
+            alignSelf: 'center',
+            marginBottom: 30
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: colors.bgGray,
+              borderRadius: 20,
+              padding: 10
+            }}
+          >
+            <Ionicons name='account' size={80} color={colors.colorWhite} />
+          </View>
+          <View
+            style={{
+              alignSelf: 'flex-end',
+              marginEnd: -10,
+              marginTop: -25,
+              backgroundColor: colors.colorWhite,
+              padding: 5,
+              borderRadius: 20
+            }}
+          >
+            <Ionicons
+              name='square-edit-outline'
+              size={20}
+              color={colors.colorSecondary}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'space-between',
+            marginBottom: 40
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'space-evenly',
+              marginBottom: 40
+            }}
+          >
+            <Editbox
+              placeholder='Овог'
+              onChangeText={text => console.log(text)}
+            />
+            <Editbox
+              placeholder='Нэр'
+              onChangeText={text => console.log(text)}
+            />
+            <Editbox
+              placeholder='Утасны дугаар'
+              onChangeText={text => console.log(text)}
+            />
+            <Editbox
+              placeholder='Нууц үг'
+              onChangeText={text => console.log(text)}
+            />
+            <Editbox
+              placeholder='Нууц үг давтах'
+              onChangeText={text => console.log(text)}
+            />
+            <Text>
+              <Text style={styles.txt}>{'Өмнө бүртгүүлсэн бол'}</Text>
+              <Text style={({ color: '#B43CF3' }, [styles.txt])}>
+                {' энд '}
+              </Text>
+              <Text style={styles.txt}>{'дарна уу.'}</Text>
+            </Text>
+          </View>
+
+          <GradientBtn
+            linearGradientStyle={styles.btnStyle}
+            onPress={() => {
+              console.log('hello world');
+            }}
+            text='Бүртгүүлэх'
+          />
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Sign up screen</Text>
-    </View>
+    <LayoutView
+      bottom={renderBottom()}
+      top={renderTop()}
+      isRoundFromTop={false}
+    />
   );
 };
 
-const styles = StyleSheet.create({});
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30
+  },
+  txt: {
+    paddingTop: 10,
+    textAlign: 'center',
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  btnStyle: {
+    borderRadius: 11
+  }
+});
 export default SignUp;
