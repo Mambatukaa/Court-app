086f8d5c62e8a59fd13d1efd4b4e45c392cb23a1
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-04-24 13:20:10 +0800

    done payment screen

--- src/screens/payment/PaymentScreen.js
+++ src/screens/payment/PaymentScreen.js
@@ -1,22 +1,118 @@
-import React from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
-import { gql, useQuery } from '@apollo/client';
-import { queries } from './graphql';
+import React, { Component, useLayoutEffect } from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { gql, useQuery, useMutation } from '@apollo/client';
+import {
+  CreditCardInput,
+  LiteCreditCardInput
+} from 'react-native-credit-card-input';
 
-const PaymentScreen = props => {
-  const { data, loading, error } = useQuery(gql(queries.currentUser), {
-    fetchPolicy: 'network-only'
-  });
+import { mutations, queries } from './graphql';
 
-  if (loading) {
-    return <ActivityIndicator size='small' color='#B43CF3' />;
-  }
+const Payment = props => {
+  const navigation = useNavigation();
+
+  const { data, loading, error } = useQuery(gql(queries.currentUser));
+
+  const currentUser = data?.currentUser;
+
+  const userId = currentUser._id;
+
+  const { courtId, scheduleId } = props.route.params;
+
+  const [booking] = useMutation(gql(mutations.bookingAdd));
+
+  const USE_LITE_CREDIT_CARD_INPUT = false;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity
+            onPress={() => {
+              booking({
+                variables: {
+                  courtId,
+                  userId,
+                  scheduleId
+                },
+                refetchQueries: ['bookingDetails', 'allSchedules']
+              })
+                .then(() => {
+                  console.log('amjilttai');
+                  navigation.navigate('Заалууд');
+                })
+                .catch(e => {
+                  console.log(e);
+                });
+            }}
+          >
+            <MaterialIcons
+              name='payment'
+              size={26}
+              style={{ color: 'white', marginRight: 15, marginTop: 4 }}
+            />
+          </TouchableOpacity>
+        </View>
+      )
+    });
+  }, [navigation]);
+
+  const onChange = formData => {
+    /* eslint no-console: 0 */
+    console.log(JSON.stringify(formData, null, ' '));
+  };
+
+  const onFocus = field => {
+    /* eslint no-console: 0 */
+    console.log(field);
+  };
 
   return (
-    <View>
-      <Text>Payment SCreen</Text>
+    <View style={s.container}>
+      {USE_LITE_CREDIT_CARD_INPUT ? (
+        <LiteCreditCardInput
+          autoFocus
+          inputStyle={s.input}
+          validColor={'black'}
+          invalidColor={'red'}
+          placeholderColor={'darkgray'}
+          onFocus={onFocus}
+          onChange={onChange}
+        />
+      ) : (
+        <CreditCardInput
+          autoFocus
+          requiresName
+          requiresCVC
+          requiresPostalCode
+          labelStyle={s.label}
+          inputStyle={s.input}
+          validColor={'black'}
+          invalidColor={'red'}
+          placeholderColor={'darkgray'}
+          onFocus={onFocus}
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
 
-export default PaymentScreen;
+const s = StyleSheet.create({
+  container: {
+    backgroundColor: '#F5F5F5',
+    marginTop: 60
+  },
+  label: {
+    color: 'black',
+    fontSize: 12
+  },
+  input: {
+    fontSize: 16,
+    color: 'black'
+  }
+});
+
+export default Payment;
--- src/screens/payment/graphql/mutations.js
+++ src/screens/payment/graphql/mutations.js
@@ -1,1 +1,12 @@
-export default {};
+const bookingAdd = `
+    mutation bookingAdd($courtId: String! $userId: String! $scheduleId: String!){
+        bookingAdd(courtId: $courtId userId: $userId scheduleId: $scheduleId ){
+            _id
+            courtId
+            userId
+            scheduleId
+        }
+    }
+`;
+
+export default { bookingAdd };
