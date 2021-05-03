55c68e76278e37ef345ccfcd7c3f9d983ddc7360
Bat-Amgalan Nasan-Ochir <batamgalan.n@nmma.co>
2021-05-03 20:32:39 +0800

    refactors

--- App.js
+++ App.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
 
 import {
   ApolloProvider,
   ApolloClient,
   HttpLink,
-  InMemoryCache,
-  split,
-  from
+  InMemoryCache
 } from '@apollo/client';
 
-import courtsReducer from './src/store/reducers/courts';
 import Root from './src/navigation/CourtNavigator';
 
-const rootReducer = combineReducers({
-  courts: courtsReducer
-});
-
-const store = createStore(rootReducer);
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
-
 const httpLink = new HttpLink({
-  uri: 'http://hurteemj.nmma.co/batukapi/graphql',
+  uri: 'http://mambacourt.codes/batukapi/graphql',
   credentials: 'include'
 });
 
